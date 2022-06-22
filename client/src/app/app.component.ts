bb0608e5421b1d0ad27c82f594de5a165fd43607
irene.ortiz <irene.ortiz@10pearls.com>
2022-06-22 15:35:36 -0500

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/users';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getUsers();
+    //this.getUsers();
+    this.setCurrentUser();
   }
 
-  private getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => this.users = response, 
-      error: (e) => console.log(e)
-  });
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
+  // private getUsers() {
+  //   this.http.get('https://localhost:5001/api/users').subscribe({
+  //     next: (response) => this.users = response, 
+  //     error: (e) => console.log(e)
+  // });
+  // }
 
 
 }
